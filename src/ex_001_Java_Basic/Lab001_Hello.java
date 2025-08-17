26d0f87845f2619be9d05c56d421db0d50f94052
firoz786-gif <pashazfiroz@gmail.com>
2025-08-17 20:30:06 +0530

    Initial commit

---
+++ src/ex_001_Java_Basic/Lab001_Hello.java
@@ -0,0 +1,7 @@
+package ex_001_Java_Basic;
+
+public class Lab001_Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/ex_001_Java_Basic/Lab002_Prb.java
@@ -0,0 +1,12 @@
+package ex_001_Java_Basic;
+
+public class Lab002_Prb {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a+b;
+        System.out.println(c);
+    }
+
+
+}
