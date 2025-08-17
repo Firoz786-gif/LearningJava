b0bc03a852b012c2256a57d808de3c66d1443508
firoz786-gif <pashazfiroz@gmail.com>
2025-08-17 21:00:19 +0530

    New add Lab003_Prb2

---
+++ src/ex_001_Java_Basic/Lab003_Prb2.java
@@ -0,0 +1,10 @@
+package ex_001_Java_Basic;
+
+public class Lab003_Prb2 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = a*b;
+        System.out.println(c);
+    }
+}
