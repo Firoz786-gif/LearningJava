1a6790c84128a8891c3ced11581434f8749c0903
firoz786-gif <pashazfiroz@gmail.com>
2025-08-17 20:56:49 +0530

    New add Lab002_Prb

--- src/ex_001_Java_Basic/Lab002_Prb.java
+++ src/ex_001_Java_Basic/Lab002_Prb.java
@@ -3,7 +3,7 @@ package ex_001_Java_Basic;
 public class Lab002_Prb {
     public static void main(String[] args) {
         int a = 1;
-        int b = 2;
+        int b = 3;
         int c = a+b;
         System.out.println(c);
     }
