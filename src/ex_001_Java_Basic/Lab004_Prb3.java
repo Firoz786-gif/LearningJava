4f7c61c615865a9ce87c26fad0a0b6c9e1da178e
firoz786-gif <pashazfiroz@gmail.com>
2025-08-17 21:32:08 +0530

    New add Lab004_Prb3

---
+++ src/ex_001_Java_Basic/Lab004_Prb3.java
@@ -0,0 +1,13 @@
+package ex_001_Java_Basic;
+
+public class Lab004_Prb3 {
+    int a = 1;
+    int b = 2;
+    int c = a+b;
+
+    public static void main(String[] args) {
+        Lab004_Prb3 c = new Lab004_Prb3();
+        System.out.println(c);
+    }
+
+}
