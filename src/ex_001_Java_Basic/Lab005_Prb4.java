5d9d10ee7ff8b6208274bf142a5bfb2ccdd72d71
firoz786-gif <pashazfiroz@gmail.com>
2025-08-30 20:06:31 +0530

    New added line

--- src/ex_001_Java_Basic/Lab005_Prb4.java
+++ src/ex_001_Java_Basic/Lab005_Prb4.java
@@ -4,6 +4,7 @@ public class Lab005_Prb4 {
     public static void main(String[] args) {
         int a = 3;
         int b= 2; //hello
+        // added
         int c = b-a;
         System.out.println(c);
 
